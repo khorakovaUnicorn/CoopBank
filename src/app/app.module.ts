9d7ebf6512d6d273568678eade8a81372704da00
khorakovaUnicorn <katerina.horakova@plus4u.net>
2022-09-21 10:44:27 +0200

    Calculator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { CalculatorFormComponent } from './calculator/calculator-form/calculator-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    CalculatorFormComponent
   ],
   imports: [
     BrowserModule
