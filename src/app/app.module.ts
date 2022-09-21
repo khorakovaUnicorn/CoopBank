db89e6004ccf8c08b0d10011bacfed5feb1123fe
khorakovaUnicorn <katerina.horakova@plus4u.net>
2022-09-21 10:19:15 +0200

    calculator - rename

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CalculationComponent } from './calculation/calculation.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculationComponent
+    CalculatorComponent
   ],
   imports: [
     BrowserModule
